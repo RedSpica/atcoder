08b498d1ad9d10c1b553368e6c97290ad26a020a
asaka <h.asaka@gxp.co.jp>
2023-07-20 12:23:16 +0900

    ローリングハッシュ完成

--- Python_classes/RollingHash.py
+++ Python_classes/RollingHash.py
@@ -1,28 +1,59 @@
-class RollingHash():
-    n=1
-    Hash1=[0]
-    # Hash2=[0]
-    Bases1=[1]
-    InvBase1=[1]
+def REV(s):
+    return ''.join(reversed(s))
 
-    S=''
+# lower case
+class RollingHash():
     mod1=998244353
-    modinv1=pow(mod1,mod1-2,mod)
-    # mod2=3
+    modinv1=pow(27,mod1-2,mod1)
+
+    mod2=10**8-11
+    modinv2=pow(27,mod2-2,mod2)
 
     def __init__(self,s):
         self.S=s
-        self.n=len(s)
+        self.N=len(s)
+
+        self.Hash1=[0]
+        self.Hash2=[0]
+
+        self.Bases1=[1]
+        self.Bases2=[1]
+
+        self.InvBase1=[1]
+        self.InvBase2=[1]
+
         Base1=1
+        Base2=1
+
         Inv1=1
+        Inv2=1
+
         now1=0
-        for i in range(n):
-            now1=(now1+(ord(S[i])-96)*Base1)%mod1
-            Hash1.append(now1)
+        now2=0
 
-            Base1=(Base1*27)%mod1
-            Bases1.append(Base1)
+        for i in range(self.N):
+            # 1
+            now1=(now1+(ord(self.S[i])-96)*Base1)%self.mod1
+            self.Hash1.append(now1)
 
-            Inv1=(Inv1*modinv1)%mod1
-            InvBase1.append(Inv1)
-             
+            Base1=(Base1*27)%self.mod1
+            self.Bases1.append(Base1)
+
+            Inv1=(Inv1*self.modinv1)%self.mod1
+            self.InvBase1.append(Inv1)
+
+            # 2
+            now2=(now2+(ord(self.S[i])-96)*Base2)%self.mod2
+            self.Hash2.append(now2)
+
+            Base2=(Base2*27)%self.mod2
+            self.Bases2.append(Base2)
+
+            Inv2=(Inv2*self.modinv2)%self.mod2
+            self.InvBase2.append(Inv2)
+
+    def get1(self,pos,size):
+        return (self.Hash1[pos+size]-self.Hash1[pos])*self.InvBase1[pos]%self.mod1
+    
+    def get2(self,pos,size):
+        return (self.Hash2[pos+size]-self.Hash2[pos])*self.InvBase2[pos]%self.mod2
