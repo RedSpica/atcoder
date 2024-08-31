c7ec17c7df8163db886cc44af00eccc791ad149c
RedSpica <asakaakasaka@icloud.com>
2024-09-01 01:16:37 +0900

    Dijkstra法の最短経路を計算する際に，復元パートを追加

--- Python_classes/Dijkstra.py
+++ Python_classes/Dijkstra.py
@@ -1,9 +1,11 @@
 import heapq
 
 class Dijkstra():
-    def __init__(self,N,G,S=0):
+    def __init__(self,N,G,S=0,restoreflg=False):
         self.n=N
         self.dist=[float('inf')]*N
+        self.prev=[-1]*N
+        self.prev_num=[-1]*N
 
         self.dist[S]=0
         self.Q=[]
@@ -25,6 +27,23 @@ class Dijkstra():
                 if self.dist[u]+cost<self.dist[x]:
                     self.dist[x]=self.dist[u]+cost
                     heapq.heappush(self.Q,(self.dist[x],x))
-    
+
+                    if restoreflg:
+                        self.prev[x]=u
+
+                        egdenum=g[2]
+                        self.prev_num[x]=egdenum
+
     def getdist(self,x):
         return self.dist[x]
+
+    def getrestore(self,goal):
+        idx=goal
+        ans=[]
+        while idx!=-1:
+            ans.append(self.prev_num[idx])
+            idx=self.prev[idx]
+
+        ans.reverse()
+
+        return ans[1:]
